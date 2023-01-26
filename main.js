b8af398be2a005b71a3b85578381323bfaf590ed
PatrickyLucas <patrickylucas@hotmail.com>
2023-01-26 04:19:42 -0300

    implementando laço for

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    contador += 1;
-
-    console.log(contador)
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
 }
