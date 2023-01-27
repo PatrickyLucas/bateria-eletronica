2042c9c878a6574cf0a5f1d95d9788036a235989
PatrickyLucas <patrickylucas@hotmail.com>
2023-01-27 04:09:41 -0300

    refinando o if da função toca som

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
     
-    if (elemento === null) {
-        console.log('Elemento não encontrado')
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido!');
     }
 
-    if (elemento != null) {
-        
-        if (elemento.localName === 'audio') {
-            elemento.play();
-        }
-    }
+ 
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
