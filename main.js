5834e4ec75057f60a73f3d9b55a5efb4fe2f275e
PatrickyLucas <patrickylucas@hotmail.com>
2023-01-27 03:34:53 -0300

    adicionando condições as funções de evento

--- main.js
+++ main.js
@@ -11,6 +11,19 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+    
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
