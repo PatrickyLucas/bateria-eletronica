929b21e6ec571c9e6b43025ba88434c11ed8a1da
PatrickyLucas <patrickylucas@hotmail.com>
2023-01-27 04:00:03 -0300

    condições de teclas

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento === null) {
+        console.log('Elemento não encontrado')
+    }
+
+    if (elemento != null) {
+        
+        if (elemento.localName === 'audio') {
+            elemento.play();
+        }
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,9 +27,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     tecla.onkeydown = function (evento) {
 
-        console.log(evento.code == 'Space')
-
-        if (evento.code === 'Space') {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
     }
